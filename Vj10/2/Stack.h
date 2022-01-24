e260bd7b7a6085473221a04117823a53cbb75080
Luka48702 <92323861+Luka48702@users.noreply.github.com>
2022-01-24 17:06:16 +0100

    Update Stack.h

--- Vj10/2/Stack.h
+++ Vj10/2/Stack.h
@@ -1,70 +1,52 @@
 #pragma once
-#include<iostream>
+#include <vector>
+#include <iostream>
+
 using namespace std;
 
 template<typename temp>
+class stack {
 
-class Stack {  //Priv
-	temp* arr;
-	size_t size;
-	size_t cap;
+	temp v;
+	int t;
 
 public:
-	Stack(size_t cap);
-	~Stack() { 
-		delete[] arr; 
-	}
-	void push(const temp& number);
-	void print();
-	void pop();
-	bool isEmpty();
+	stack();
+	void push(temp v);
+	temp pop();
+	bool is_empty();
+	virtual ~stack();
 };
 
+template<typename temp>
+stack<temp>::stack() {
+	this->v = temp[15];
+	t = 0;
+}
+
+template<typename X>
+stack<X>::~stack() {
+	delete[] v;
+}
+
+template<typename temp>
+void stack<temp>::push(temp number) {
+	if (!(t == 11 - 1))
+		v[++t] = number;
+	else
+		cout << "Full" << endl;
+}
+template<typename temp>
+bool stack<temp>::is_empty() {
+	if (t == 0)
+		return true;
+	return false;
+}
 template <typename temp>
-Stack<temp>::Stack(size_t v)
+temp stack<temp>::pop()
 {
-	arr = new int[v];
-	size = 0;
-	cap = v;
-}
-
-template <typename temp>
-void Stack<temp>::push(const temp& number) {
-	if (size == cap)
-	{
-		cap *= 2;
-		int* myArr = new T[cap];
-
-		for (int i = 0; i < cap - 1; i++)
-		{
-			myArr[i] = arr[i];
-		}
-
-		delete[] arr;
-		arr = myArr;
-	}
-
-	arr[size] = number;
-	++size;
-}
-
-template <typename temp>
-void Stack<temp>::pop() {
-	if (!size) 
-		return;
-	else {
-		--size;
-	}
-}
-
-template <typename temp>
-bool Stack<temp>::isEmpty() {
-	return size;
-}
-
-template <typename temp>
-void Stack<temp>::print() {
-	for (int i = 0; i < size; i++) {
-		cout << arr[i] << " ";
-	}
+	if (!is_empty())
+		return v[t--];
+	else
+		cout << "Empty" << endl;
 }
